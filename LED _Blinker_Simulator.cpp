87f0e08b65065a21cad5ce8291592d5ee6d20822
Preenu Ann <preenu91@gmail.com>
2025-08-28 23:05:29 +0100

    Add project files.

---
+++ LED _Blinker_Simulator.cpp
@@ -0,0 +1,47 @@
+﻿// Digital LED Blinker Simulator.cpp : Defines the entry point for the application.
+//
+
+#include "LED_Blinker_Simulator.h"
+
+
+int main()
+{
+	int numBlinkCycles{}, interval{};
+	std::cout << "Enter the number of blink cycles\n";
+	std::cin >> numBlinkCycles;
+	std::cout << "Enter the interval in milliseconds between LED turning ON and OFF\n";
+	std::cin >> interval;
+
+	LED led1{true};
+	led1.blink(numBlinkCycles, interval);
+
+	return 0;
+}
+
+void LED::turnOn()
+{
+	m_state = true;
+	showState();
+}
+void LED::turnOff()
+{
+	m_state = false;
+	showState();
+}
+
+void LED::blink(int cycles, int interval)
+{
+	for (int i{ 0 }; i < cycles; ++i)
+	{
+		turnOn();
+		std::this_thread::sleep_for(std::chrono::milliseconds(interval)); 
+		turnOff();
+		std::this_thread::sleep_for(std::chrono::milliseconds(interval));
+	}
+
+}
+
+void LED::showState() const
+{
+	m_state ? std::cout << "LED ON\n" : std::cout << "LED OFF\n";
+}
---
+++ LED_Blinker_Simulator.h
@@ -0,0 +1,28 @@
+﻿// Digital LED Blinker Simulator.h : Include file for standard system include files,
+// or project specific include files.
+
+#ifndef _LED_BLINKER_SIMULATOR
+#define _LED_BLINKER_SIMULATOR
+
+#include<iostream>
+#include <thread>
+#include <chrono>
+
+class LED
+{
+private:
+	bool m_state{}; //state of LED 
+
+public:
+	LED(bool state) : m_state{ state } {}; //Constructor
+	void turnOn();							// turn LED ON
+	void turnOff();							// turn LED OFF
+	void blink(int cycles, int interval);	/* turn LED ON and OFF with 'interval' time difference
+												and repeat 'cycles' number of times*/
+	void showState() const;
+};
+
+#endif
+
+
+
